d6d8b41a8d4542c8539beba4c85df7c70edaa7e5
AFEEZ ADEREMI ADEOLA <hardeyreemyarfyc@gmail.com>
2022-07-08 12:24:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const client = contentful.createClient({
+  // Contentfull configuration
   // This is the space ID. A space is like a project folder in Contentful terms
   space: '5iedh4ksjfvc',
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
